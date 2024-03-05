33ecbc6094a2762daa2c4488cfa5ac2f58290b10
Mike_Jackson <mikejackson35@gmail.com>
2024-03-05 09:29:04 -0800

    update

--- app.py
+++ app.py
@@ -95,25 +95,25 @@ def highlight_rows(row):
         color = '#a5aa99' # Grey
     return ['background-color: {}'.format(color) for r in row]
 
-# def highlight_rows2(row):
-#     value = row.index()
-#     if value == 'unit_circle':
-#         color = '#FFCCE5' # Pink
-#     elif value == 'Philly919':
-#         color = '#7f3c8d' # Purple
-#     elif value == 'AlphaWired':
-#         color = '#3969ac' # Blue
-#     elif value == 'Sneads Foot':
-#         color = '#f2b701' # Gold
-#     elif value == 'New Team 4':
-#         color = '#e73f74' # Magenta
-#     elif value == 'Team Gamble':
-#         color = '#e68310' # Orange
-#     elif value == 'txmoonshine':
-#         color = '#00868b' # Aqua
-#     else:
-#         color = '#a5aa99' # Grey
-#     return ['background-color: {}'.format(color) for r in row]
+def highlight_rows2(row):
+    value = row.loc['Team']
+    if value == 'unit_circle':
+        color = '#FFCCE5' # Pink
+    elif value == 'Philly919':
+        color = '#7f3c8d' # Purple
+    elif value == 'AlphaWired':
+        color = '#3969ac' # Blue
+    elif value == 'Sneads Foot':
+        color = '#f2b701' # Gold
+    elif value == 'New Team 4':
+        color = '#e73f74' # Magenta
+    elif value == 'Team Gamble':
+        color = '#e68310' # Orange
+    elif value == 'txmoonshine':
+        color = '#00868b' # Aqua
+    else:
+        color = '#a5aa99' # Grey
+    return ['background-color: {}'.format(color) for r in row]
 
 
 live_merged['holes_remaining'] = (72 - (live_merged['thru']).fillna(0))
