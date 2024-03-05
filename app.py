c31b644136ca5d17e6ff92a80cb6d5218ecb52d6
Mike_Jackson <mikejackson35@gmail.com>
2024-03-05 09:16:56 -0800

    update

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ thru_cut = pd.DataFrame(live_merged[live_merged.position !='CUT']['team'].value_
 df_holes_remaining = pd.DataFrame(live_merged.groupby('team')['holes_remaining'].sum())
 
 table = pd.merge(thru_cut,df_holes_remaining, left_index=True, right_index=True)
-table = table.merge(team_score, left_index=True, right_index=True).rename(columns={'team':'Thru Cut','holes_remaining':'Holes Remaining','total':'Team Score'})
+table = table.merge(team_score, left_index=True, right_index=True).rename(columns={'count':'Thru Cut','team':'Team','holes_remaining':'Holes Remaining','total':'Team Score'})
 
 st.write("")
 st.header('Arnold Palmer Invitational')
