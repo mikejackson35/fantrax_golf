b9c26d4c1845f11a89c342f5a633866a17782717
Mike_Jackson <mikejackson35@gmail.com>
2024-03-07 07:43:04 -0800

    update

--- app.py
+++ app.py
@@ -33,6 +33,8 @@ names = live['player'].str.split(expand=True)
 names[0] = names[0].str.rstrip(",")
 names[1] = names[1].str.rstrip(",")
 names['player'] = names[1] + " " + names[0]
+names['player'] = np.where(names['player']=='Matt Fitzpatrick', 'Matthew Fitzpatrick', names['player'])
+names['player'] = np.where(names['player']=='Si Kim', 'Si Woo Kim', names['player'])
 names['player'] = np.where(names['player']=='Min Lee', 'Min Woo Lee', names['player'])
 names['player'] = np.where(names['player']=='Byeong An', 'Byeong Hun An', names['player'])
 names['player'] = np.where(names['player']=='Rooyen Van', 'Erik Van Rooyen', names['player'])
